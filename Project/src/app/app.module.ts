36a03952a78871e1a81b8ce916fb0a9b12649853
Anzori-PR <saxelashvilianzori@gmail.com>
2024-02-01 22:31:16 +0400

    Dashboard Design

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Project/src/app/component/dashboard/dashboard.component.ts
+++ Project/src/app/component/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { rocket_launches } from 'src/app/rocket_launches.interface';
 import { FetchDataService } from 'src/app/service/fetch-data.service';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +13,24 @@ import { FetchDataService } from 'src/app/service/fetch-data.service';
 })
 export class DashboardComponent implements OnInit{
   Data: rocket_launches[] = [];
+  menu: boolean = true;
+  close = 200;
+  faRocket = faRocket;
+  faCompass = faCompass;
+  faBookmark = faBookmark;
+  faSearch = faSearch;
 
   constructor(private service : FetchDataService) {}
 
   ngOnInit(): void {
     this.service.getData().subscribe( (data) => {
       this.Data = data;
-      console.log(this.Data)
     })
+  }
+
+  sideMenu() {
+    this.close = this.close === 200 ? 40 : 200;
+    this.menu = !this.menu;
   }
 
 
